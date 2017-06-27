874c1ce6f048124bbc8e35f9b909861ec32a1c4c
Daniel Romero <raiga1234@gmail.com>
2017-06-26 20:05:26 -0500

    Test Service Worker

---
+++ ProgressiveWebApp/assets/service-Worker/sw.js
@@ -0,0 +1,38 @@
+
+const CACHE_NAME = "achievements-v1";
+const CACHE_URLS = [
+    "/assets/styles/indigo-pink.css"
+];
+
+self.addEventListener('install', function (ev) {
+    caches.open(CACHE_NAME)
+        .then(function (cache) {
+            console.log("Cache abierto");
+            return cache.addAll(CACHE_URLS);
+        })
+        .catch(err => console.log(err));
+
+});
+
+self.addEventListener('activate', function () { });
+
+self.addEventListener('fetch', function (ev) {
+    ev.respondWith(
+        caches.match(ev.request)
+            .then(function (response) {
+                // response es la respuesta de buscar en el cache
+                console.log(ev.request.url);
+                if (response) {
+                    console.log("Estoy en el cache y te ahorré una petición");
+                    return response;
+                } else {
+                    console.log("No estoy en el cache :3");
+                }
+                // Ve al servidor
+                return fetch(ev.request);
+
+            })
+            .catch(er=>console.log(er))
+
+    )
+});
