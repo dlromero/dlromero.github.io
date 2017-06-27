66962851917267740ac3d697d128cda8a8766b1f
Daniel Romero <raiga1234@gmail.com>
2017-06-26 20:20:04 -0500

    Test Service Worker VII

--- ProgressiveWebApp/assets/service-Worker/sw.js
+++ ProgressiveWebApp/assets/service-Worker/sw.js
@@ -1,7 +1,7 @@
 
 const CACHE_NAME = "achievements-v1";
 const CACHE_URLS = [
-    "../styles/indigo-pink.css"
+    "/assets/styles/indigo-pink.css"
 ];
 
 self.addEventListener('install', function (ev) {
@@ -16,23 +16,37 @@ self.addEventListener('install', function (ev) {
 
 self.addEventListener('activate', function () { });
 
-self.addEventListener('fetch', function (ev) {
-    ev.respondWith(
-        caches.match(ev.request)
-            .then(function (response) {
-                // response es la respuesta de buscar en el cache
-                console.log(ev.request.url);
-                if (response) {
-                    console.log("Estoy en el cache y te ahorré una petición");
-                    return response;
-                } else {
-                    console.log("No estoy en el cache :3");
-                }
-                // Ve al servidor
-                return fetch(ev.request);
+// self.addEventListener('fetch', function (ev) {
+//     ev.respondWith(
+//         caches.match(ev.request)
+//             .then(function (response) {
+//                 // response es la respuesta de buscar en el cache
+//                 console.log(ev.request.url);
+//                 if (response) {
+//                     console.log("Estoy en el cache y te ahorré una petición");
+//                     return response;
+//                 } else {
+//                     console.log("No estoy en el cache :3");
+//                 }
+//                 // Ve al servidor
+//                 return fetch(ev.request);
 
-            })
-            .catch(er=>console.log(er))
+//             })
+//             .catch(er=>console.log(er))
 
+//     )
+// });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
     )
+  );
 });
